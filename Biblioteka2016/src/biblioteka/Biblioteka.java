7ec98c600ddd67fb9ec7a1f700e97e68a0f2a051
srdjansubotic <srdjansubotic@yahoo.com>
2016-03-17 14:47:54 +0100

    Uvedena logicka kontrola u klasu Biblioteka.

--- Biblioteka2016/src/biblioteka/Biblioteka.java
+++ Biblioteka2016/src/biblioteka/Biblioteka.java
@@ -2,6 +2,8 @@ package biblioteka;
 
 import java.util.LinkedList;
 
+import javax.management.RuntimeErrorException;
+
 import biblioteka.interfejs.BibliotekaInterfejs;
 
 public class Biblioteka implements BibliotekaInterfejs {
@@ -9,13 +11,17 @@ public class Biblioteka implements BibliotekaInterfejs {
 	private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
 
 	public void dodajKnjigu(Knjiga knjiga) {
-
+		if(knjiga==null || knjige.contains(knjiga)){
+			throw new RuntimeException("Greska pri unosu knjige");
+		}
 		knjige.add(knjiga);
 
 	}
 
 	public void obrisiKnjigu(Knjiga knjiga) {
-
+		if(knjiga==null || !knjige.contains(knjiga)){
+			throw new RuntimeException("Greska pri unosu knjige");
+		}
 		knjige.remove(knjiga);
 
 	}
@@ -27,6 +33,11 @@ public class Biblioteka implements BibliotekaInterfejs {
 	}
 
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		if(naslov==null || naslov.isEmpty()){
+			throw new RuntimeException("Naslov je prazan!");
+		}
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 
 		for (int i = 0; i < knjige.size(); i++) {
